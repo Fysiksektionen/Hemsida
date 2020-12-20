6652dc47549c08d5b4b0d22d869f0a35e522869e
Acksell <axelen@kth.se>
2020-12-20 22:03:29 +0100

    Added comment about reportWebVitals.ts

--- client/src/reportWebVitals.ts
+++ client/src/reportWebVitals.ts
@@ -1,5 +1,6 @@
 import { ReportHandler } from 'web-vitals';
 
+// Provide somewhat standardised metrics that are scraped by Google for example
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
