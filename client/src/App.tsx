b79313ccffd173af50e61baad264033cc67e8198
Axel Engström <axelen@kth.se>
2020-12-20 22:16:36 +0100

    Create react app (#1)
    * Added create-react-app boilerplate
    
    * Updated README slightly.
    
    * Added fysiksektionen logo instead :)
    
    * Added .eslintcache to .gitignore
    
    * Added comment about reportWebVitals.ts

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './Fysiksektionen_logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Tja F.dev! Redigera <code>client/src/App.tsx</code> och hacka på!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
