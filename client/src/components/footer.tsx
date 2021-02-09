5d91c533efcc02e65645d549c31c41c54cc74110
Morris E <43953171+Morriser@users.noreply.github.com>
2021-02-09 23:31:56 +0100

    Frontend frontpage, footer and header (#9)
    * Added simple frontpage with header and footer.
    
    * Added placeholder image on frontpage
    
    * Updated footer placeholder
    
    * New favicon

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './footer.css'
+
+function Footer() {
+    
+    return (
+        <div className="container-fluid py-4 bg-dark text-white text-center">
+
+            <h3>Hitta hit</h3>
+
+            Brinellvägen 89, 114 28 Stockholm
+
+        </div>
+    )
+}
+
+export default Footer;
