40be6bb4cf39979b2f11cbb03dfad5f19238d8e8
Christoffer Ejemyr <ejemyr@kth.se>
2021-02-09 01:45:31 +0100

    Added casting methods allowing for easy exctraction of object of correct proxy-type.

--- server/website/services/__init__.py
+++ server/website/services/__init__.py
@@ -2,3 +2,5 @@
 A service is a function performing actions on the database. Creating, updating or deleting objects. They contain logic
 around permissions and validation.
 """
+
+# TODO: Add service for type conversion of MenuItemBase To Menu and MenuItem.
