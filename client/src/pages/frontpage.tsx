14f2f37ff824d49f625a64c6d8c46d999b65acfa
morriser <morriser@kth.se>
2021-02-09 22:08:26 +0100

    Added placeholder image on frontpage

--- client/src/pages/frontpage.tsx
+++ client/src/pages/frontpage.tsx
@@ -1,9 +1,10 @@
+import placeholder from '../placeholder.jpg'
 import React from 'react';
 
 function Frontpage() {
   return (
     <div className="" style={{height: "800px"}}>
-        Here be content
+        <img src={placeholder} style={{width: "100%", marginTop: "50px"}}></img>
     </div>
   )
 }
