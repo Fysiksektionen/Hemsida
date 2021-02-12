d3731f1356473ff4b8d3091526d65bea7ba8ec5e
fridaagronberg <frida_gron@hotmail.com>
2021-02-12 22:17:56 +0100

    added function get_content

--- server/website/models/pages.py
+++ server/website/models/pages.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
-from content_objects import ContentObjectBase
 
 
 class PageDraft(models.Model):
@@ -9,11 +8,14 @@ class PageDraft(models.Model):
     Model for a Page draft.
     """
     page_type = models.CharField(verbose_name=_('page type'), max_length=255)
-    content_sv = models.ForeignKey('ContentObjectBase', verbose_name=_('swedish content'), blank=True, null=True,
-                                   on_delete=models.SET_NULL)
-    content_en = models.ForeignKey('ContentObjectBase', verbose_name=_('english content'), blank=True, null=True,
-                                   on_delete=models.SET_NULL)
+    content_sv = models.ForeignKey(
+        'ContentObjectBase', verbose_name=_('swedish content'), blank=True, null=True, on_delete=models.SET_NULL
+    )
+    content_en = models.ForeignKey(
+        'ContentObjectBase', verbose_name=_('english content'), blank=True, null=True, on_delete=models.SET_NULL
+    )
     last_edited_at = models.DateField(verbose_name=_('last edited at'), null=False, blank=False, auto_now_add=True)
+
 
 class Page(models.Model):
     """
@@ -25,16 +27,18 @@ class Page(models.Model):
     url = models.URLField(verbose_name=_('URL'), blank=False, null=False)
     name = models.CharField(verbose_name=_('name'), max_length=255)
     page_type = models.CharField(verbose_name=_('page type'), max_length=255)
-    parent = models.ForeignKey('Page', verbose_name=_('parent page'), blank=True, null=True, on_delete=models.SET_NULL)
+    parent = models.ForeignKey(
+        'Page', verbose_name=_('parent page'), blank=True, null=True, on_delete=models.SET_NULL
+    )
     slug = models.SlugField(verbose_name='Slug', null=False, blank=False)
 
-    page_draft = models.OneToOneField(verbose_name=_('page draft'), null=True, blank=True)
+    page_draft = models.OneToOneField(
+        'PageDraft', verbose_name=_('page draft'), null=True, blank=True, on_delete=models.SET_NULL
+    )
     has_draft = models.BooleanField(verbose_name=_('has draft'))
-
     published = models.BooleanField(verbose_name=_('is published'))
     published_at = models.DateField(verbose_name=_('published at'), null=True, blank=True, auto_now_add=False)
     last_edited_at = models.DateField(verbose_name=_('last edited at'), null=False, blank=False, auto_now_add=True)
-
     content_sv = models.ForeignKey('ContentObjectBase', verbose_name=_('swedish content'), blank=True, null=True,
                                    on_delete=models.SET_NULL)
     content_en = models.ForeignKey('ContentObjectBase', verbose_name=_('english content'), blank=True, null=True,
@@ -49,3 +53,12 @@ class Page(models.Model):
                 kwargs['slug'] = ''
         super().__init__(*args, **kwargs)
 
+    def get_content(self, language):
+        if not isinstance(language, str):
+            raise TypeError(_("Request must be string."))
+        elif language == 'sv':
+            return self.content_sv
+        elif language == 'en':
+            return self.content_en
+        else:
+            raise ValueError(_("Request is invalid, must be 'sv' or 'en'."))
---
+++ server/website/tests/test_pages.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+from website.models.pages import Page, PageDraft
+from website.models.content_objects import ContentObjectBase
+
+from .utils import ValidationTestCase
+
+
