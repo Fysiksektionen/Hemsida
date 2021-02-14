bf53ba00188059761d54ab46507ea730b878c4b4
gustavvk <gustav.vk1@gmail.com>
2021-02-14 21:21:42 +0100

    fixed temp button onclick

--- client/src/components/button.tsx
+++ client/src/components/button.tsx
@@ -4,13 +4,14 @@ import Button from 'react-bootstrap/Button';
 export interface IButton {
     text: string,
     type: string,
-    class?: string
+    class?: string,
+    onClick?: Function,
 }
 
 function OurButton(props: IButton) {
     // TODO: Temporary, should be improved in another branch
     return (
-        <Button className={props.class} variant={props.type}>{props.text}</Button>
+        <Button className={props.class} variant={props.type} onClick={() => {if(props.onClick) props.onClick()}}>{props.text}</Button>
     )
 }
 
