459c1d3b6b46ded0203eb0173e90861df0268a4f
Christoffer Ejemyr <ejemyr@kth.se>
2021-02-21 01:41:01 +0100

    Initialized test files.

---
+++ server/authentication/tests/models/test_users.py
@@ -0,0 +1,9 @@
+from utils.tests import ValidationTestCase
+
+
+class UserModelTest(ValidationTestCase):
+    """Test the functionality of the User model."""
+
+    def setUp(self):
+        """Creation of objects"""
+        pass
