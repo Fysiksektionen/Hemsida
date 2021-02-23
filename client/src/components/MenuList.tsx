54446c8c7797c313153a6f88b34076950d2db965
afinnson <50794588+afinnson@users.noreply.github.com>
2021-02-23 10:30:01 +0100

    Header menu (#24)
    * Menu sidebar without clickable links
    
    * removed example code and improved styling
    
    * added x for closing
    
    * changed SidebarMenu to HeaderMenu
    
    Co-authored-by: Anton Finnson <afinnson@kth.se>

---
+++ client/src/components/MenuList.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { List, ListItem } from "@material-ui/core";
+import { IMenuItem } from "./HeaderMenu";
+
+<div
+  className="bg-dark text-white px-4"
+  style={{
+    height: "100%",
+    minWidth: window.innerWidth * 0.4
+  }}
+>
+  Text
+</div>
+
+interface IMenuListProps {
+  data: IMenuItem[];
+}
+
+export default function MenuList({ data }: IMenuListProps ) {
+  return (
+    <div
+      className="bg-dark text-white"
+      style={{
+        height: "100%",
+        minWidth: window.innerWidth * 0.4
+      }}
+    >
+      <List>
+        {data.map(val => (
+            <ListItem>
+              <div 
+                style={{
+                  fontWeight: "bold",
+                  fontSize: val.isHeader ? "170%" : "100%",
+                  marginLeft: val.isHeader ? "3rem" : "4rem",
+                  marginTop: val.isHeader ? "1rem" : 0 
+                }}
+              >
+                {val.itemText}
+              </div>
+            </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
