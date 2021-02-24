a4b6733a82a0837bd6c2e2b20cbd5084e92400f2
Christoffer Ejemyr <ejemyr@kth.se>
2021-02-24 14:36:38 +0100

    Fixed migrations so  groups are initialized before ForeginKey is added.

--- server/authentication/migrations/0003_backend-group-model_20210222.py
+++ server/authentication/migrations/0003_backend-group-model_20210222.py
@@ -31,4 +31,13 @@ class Migration(migrations.Migration):
                 ('objects', django.contrib.auth.models.GroupManager()),
             ],
         ),
+
+        migrations.AlterField(
+            model_name='user',
+            name='groups',
+            field=models.ManyToManyField(blank=True,
+                                         help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                                         related_name='user_set', related_query_name='user', to='authentication.Group',
+                                         verbose_name='groups'),
+        ),
     ]
