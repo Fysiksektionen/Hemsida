41077c4ab2c518ae6ff71273dec29f88a0a9ec41
Morris E <43953171+Morriser@users.noreply.github.com>
2021-02-26 17:02:59 +0100

    Locale access through React Context (#34)

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,4 +1,5 @@
-import { Button, Container } from '@material-ui/core';
+import { Button, Container, Select } from '@material-ui/core';
+import { LocaleContext, locales } from '../contexts'
 import './header.css'
 import React, { useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
@@ -7,27 +8,43 @@ import { GroupedSearch } from './search-box';
 import HeaderMenu from './HeaderMenu';
 
 
-function Header() {
-  return (
-    <div 
-      className="navbar sticky-top bg-light px-4" 
-      style={{
-        display: "flex",
-        justifyContent: "space-between"
-      }}
-    >
-        <a className="navbar-brand mx-5 text-center" href="/">
-            <img src={logo} width="80" height="80" alt="" />
-            <h2>Fysiksektionen</h2>
-        </a>
-        <div>
-          <div className="mx-4">
-            <GroupedSearch/>
-          </div>
-          <HeaderMenu/>
-        </div>
-      </div>
-  );
+type Props = {
+    setLocale: Function
+}
+
+function Header(props: Props) {
+    return (
+        <LocaleContext.Consumer>
+        {locale => 
+            <div 
+            className="navbar sticky-top bg-light px-4" 
+            style={{
+                display: "flex",
+                justifyContent: "space-between"
+            }}
+            >
+            <a className="navbar-brand mx-5 text-center" href="/">
+                <img src={logo} width="80" height="80" alt="" />
+                <h2>Fysiksektionen</h2>
+            </a>
+            <div>
+            <div className="mx-4">
+                <GroupedSearch/>
+            </div>
+            <HeaderMenu/>
+            </div>
+
+                <Select value={locale.id} onChange={event => {
+                    props.setLocale(locales[event.target.value as string])
+                }}>
+                    {Object.keys(locales).map(key => 
+                        <option value={key}>{locales[key].name}</option>
+                    )}
+                </Select>
+
+        </div>}
+        </LocaleContext.Consumer>
+    );
 }
 
 export default Header;
---
+++ client/src/contexts.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type Locale = {
+    id: string,
+    name: string
+}
+
+export const locales: {[key: string]: Locale} = {
+    sv: {
+        id: "sv",
+        name: "Svenska"
+    },
+    en: {
+        id: "en",
+        name: "English"
+    }
+}
+
+export const LocaleContext = React.createContext(
+    locales.sv     // Default
+)
