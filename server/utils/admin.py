38dc351abb203ef2033c9a0545aac6e97605e6c3
Christoffer Ejemyr <ejemyr@kth.se>
2021-02-28 12:51:33 +0100

    Added docstrings to GuardedModelAdmin.

--- server/utils/admin.py
+++ server/utils/admin.py
@@ -1,14 +1,21 @@
-from django.contrib import auth
 from django.contrib.auth import get_permission_codename
 from guardian.admin import GuardedModelAdmin as OriginalGuardedModelAdmin
 
 
 class GuardedModelAdmin(OriginalGuardedModelAdmin):
-    def has_add_permission(self, request):
-        codename = get_permission_codename('add', self.opts)
-        return request.user.has_perm("%s.%s" % (self.opts.app_label, codename))
+    """Extending the GuardedModelAdmin from django-guardian package.
+
+    Adds functionality to allow certain users view, add, change or remove
+    stuff based on both model and object permissions.
+    """
 
     def has_change_permission(self, request, obj=None):
+        """
+        Return True if the given request has permission to change the given
+        Django model instance. Checks if user has either general permissions
+        or object specific permissions.
+        """
+
         codename = get_permission_codename('change', self.opts)
         return (
             request.user.has_perm("%s.%s" % (self.opts.app_label, codename)) or
@@ -16,6 +23,12 @@ class GuardedModelAdmin(OriginalGuardedModelAdmin):
         )
 
     def has_delete_permission(self, request, obj=None):
+        """
+        Return True if the given request has permission to delete the given
+        Django model instance. Checks if user has either general permissions
+        or object specific permissions.
+        """
+
         codename = get_permission_codename('delete', self.opts)
         return (
             request.user.has_perm("%s.%s" % (self.opts.app_label, codename)) or
@@ -23,6 +36,13 @@ class GuardedModelAdmin(OriginalGuardedModelAdmin):
         )
 
     def has_view_permission(self, request, obj=None):
+        """
+        Return True if the given request has permission to view the given
+        Django model instance. Checks if user has either general permissions
+        or object specific permissions.
+
+        If a user has change permissions the view permission is granted.
+        """
         codename_view = get_permission_codename('view', self.opts)
         codename_change = get_permission_codename('change', self.opts)
         return (
