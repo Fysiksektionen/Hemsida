9045e238f4e0c980563c0dbd0be20febaff01e29
Christoffer Ejemyr <ejemyr@kth.se>
2021-02-28 14:58:22 +0100

    Simple views for redirects.py.

--- server/website/urls.py
+++ server/website/urls.py
@@ -1,13 +1,18 @@
 from rest_framework.routers import DefaultRouter
 from website.views.menus import MenuViewSet
+from website.views.redirects import RedirectViewSet
 
 app_name = 'website'
 
 # Routs/urls for menus and menu-items
 menu_router = DefaultRouter()
-menu_router.register(r'menus', MenuViewSet, basename='menu')  # menu-list
+menu_router.register(r'menus', MenuViewSet, basename='menu')
+
+redirect_router = DefaultRouter()
+redirect_router.register(r'redirects', RedirectViewSet, basename='redirect')
 
 # Url-patterns for website app.
 urlpatterns = [
-    *menu_router.urls
+    *menu_router.urls,
+    *redirect_router.urls
 ]
