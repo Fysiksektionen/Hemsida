5a051962bffbd918ee5209ed468d072bc328cac1
Christoffer Ejemyr <ejemyr@kth.se>
2021-03-05 03:03:19 +0100

    Serving API-docs in site on /api-docs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import { Switch, Route } from "react-router-dom"
 import './App.css'
 import {DummyData2} from './components/news/FrontpageNewsWidget'
 import StyretPage from './pages/StyretPage'
-import APIServe from "./components/APIServe";
+import APIDocs from "./pages/APIDocs/APIDocs";
 
 function App() {
 
@@ -17,31 +17,28 @@ function App() {
     return (
         <div className="App">
             <LocaleContext.Provider value={locale}>
-                <Switch>
-                    <Route path="/api-docs/authentication">
-                        <APIServe url="http://localhost:3001/authentication.json" />
-                    </Route>
-                    <Route path="/api-docs/website">
-                        <APIServe url="http://localhost:3001/website.json" />
-                    </Route>
-                    <Route path="*">
-                        <Header setLocale={setLocale}/>
-                        <div className="content container">
-                            <Switch>
-                                <Route path="/styret">
-                                    <StyretPage/>
-                                </Route>
-                                <Route path="/newsarticle">
-                                    <NewsArticlePage {...DummyData2} />
-                                </Route>
-                                <Route path="/">
-                                    <Frontpage />
-                                </Route>
-                            </Switch>
-                        </div>
-                        <Footer />
-                    </Route>
-                </Switch>
+                    <Switch>
+                        <Route path="/api-docs">
+                            <APIDocs />
+                        </Route>
+                        <Route path="*">
+                            <Header setLocale={setLocale}/>
+                            <div className="content container">
+                                <Switch>
+                                    <Route path="/styret">
+                                        <StyretPage/>
+                                    </Route>
+                                    <Route path="/newsarticle">
+                                        <NewsArticlePage {...DummyData2} />
+                                    </Route>
+                                    <Route path="/">
+                                        <Frontpage />
+                                    </Route>
+                                </Switch>
+                            </div>
+                            <Footer />
+                        </Route>
+                    </Switch>
             </LocaleContext.Provider>
         </div>
     );
---
+++ client/src/pages/APIDocs/APIDocs.tsx
@@ -0,0 +1,130 @@
+import '../../index.css'
+import {Container, Navbar, Nav} from "react-bootstrap";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
+import logo from "../../Fysiksektionen_logo.svg";
+import {useEffect, useState} from "react";
+import FButton from "../../components/f-styled/buttons/FButton";
+import {timeoutPromise} from "../../utils/timeoutPromise";
+import {FormControlLabel, Switch} from "@material-ui/core";
+
+type DocsInfo = {
+    fileName: string  // Relative server root (without leading slash)
+    displayName: string  // Name in menu
+}
+
+type APIDocsProps = {
+    serverUrl?: string  // full URL with appended slash
+    docs?: DocsInfo[]
+    timeOut?: number  // ms
+    updateInterval?: number
+    autoUpdateEnabled?: boolean
+}
+
+type APIDocsState = {
+    jsonUrl: string
+    jsonData: object
+    autoUpdateEnabled: boolean
+}
+
+function APIDocs({
+                     serverUrl="http://localhost:3001/",
+                     docs=[
+                         {fileName: 'website.json', displayName: 'Website'},
+                         {fileName: 'authentication.json', displayName: 'Authentication'}
+                     ],
+                     timeOut=1000,
+                     updateInterval=2500,
+                     autoUpdateEnabled=false
+                 }: APIDocsProps) {
+
+    const [state, setState] = useState({
+        jsonUrl: "",
+        jsonData: {},
+        autoUpdateEnabled: autoUpdateEnabled
+    } as APIDocsState);
+
+    const updateAPIData = async function(jsonUrl: string) {
+        if (jsonUrl !== "") {
+            const res = await timeoutPromise(
+                fetch(jsonUrl).then(response => response.json()),
+                timeOut
+            ).catch(reason => {
+                console.log(`Request on ${jsonUrl}: ${reason.message}`);
+            });
+            if (res) {
+                if( JSON.stringify(res) !== JSON.stringify(state.jsonData) ) {
+                    setState({
+                        jsonUrl: jsonUrl,
+                        jsonData: res,
+                        autoUpdateEnabled: state.autoUpdateEnabled
+                    });
+                }
+            }
+        }
+    }
+
+    useEffect(() => {
+        const intervalId = window.setInterval(() => {
+            if( state.autoUpdateEnabled ) {
+                updateAPIData(state.jsonUrl).then();
+            }
+        }, updateInterval);
+
+        return () => {clearInterval(intervalId)}
+    });
+
+    return (
+        <div>
+            <Navbar variant="light" expand="md" className="border-bottom border-dark">
+                <Navbar.Brand>
+                    <div className="container-fluid">
+                        <a href="/"><img src={logo} width="75" height="75" alt="" className="mx-3" /></a>
+                        Fysiksektionens API-docs
+                    </div>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav justify onSelect={(eventKey) => {
+                        if( eventKey != null ) {
+                            updateAPIData(serverUrl + eventKey).then();
+                        }
+                    }}>
+                        {docs.map((docsInfo) => {
+                            return(
+                                <Nav.Link eventKey={docsInfo.fileName}>
+                                    {docsInfo.displayName}
+                                </Nav.Link>
+                            )
+                        })}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Container fluid="lg">
+                <Row className="my-3 d-flex justify-content-center">
+                    <Col xs={10} className="d-flex justify-content-around">
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={state.autoUpdateEnabled}
+                                    onChange={(event, checked) => {
+                                        setState({...state, autoUpdateEnabled: checked})
+                                    }}
+                                />
+                            }
+                            label="Automatic updates"
+                        />
+                        <FButton text="Run manual update" version="dark" onClick={() => updateAPIData(state.jsonUrl)} />
+                    </Col>
+                </Row>
+                <Row>
+                    <SwaggerUI spec={state.jsonData} />
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default APIDocs;
