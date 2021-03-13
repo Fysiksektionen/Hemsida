0dd17879c5c044b5fc32a3e511faf956ff68aa22
Christoffer Ejemyr <ejemyr@kth.se>
2021-03-13 16:28:48 +0100

    Fixed type definition on Map.

--- client/src/pages/PageTypeMap.ts
+++ client/src/pages/PageTypeMap.ts
@@ -1,12 +1,14 @@
 import Frontpage from "./Frontpage";
 import NewsArticlePage from "./NewsArticlePage";
 import StyretPage from "./StyretPage";
+import {PageData} from "../components/PageTypeLoader";
+import {FunctionComponent} from "react";
 
 
-const pageTypeMap: { [key: string]: any } = {
-    start: Frontpage,
-    news_article: NewsArticlePage,
-    styret: StyretPage
+const pageTypeMap: { [key: string]: FunctionComponent<PageData> } = {
+    "start": Frontpage,
+    "news_article": NewsArticlePage,
+    "styret": StyretPage
 }
 
 export default pageTypeMap;
