eb9c3b310d6a607aea5140924055286f8d829eb7
gustavvk <gustav.vk1@gmail.com>
2021-03-13 16:11:01 +0100

    Updated orage boxes to be a little bit better

--- client/src/components/OrangeInfoBoxes.tsx
+++ client/src/components/OrangeInfoBoxes.tsx
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Centered from './CenteredText';
+import LoremIpsum from './LoremIpsum';
+import FButton from './f-styled/buttons/FButton';
+
+
+const DummyData : InfoBoxProps = {title: "Vill du hitta på något?", text: <LoremIpsum />, button_text: "Engagera dig", bg_color: "orange"}
 
 function OrangeInfoBoxes() {
-    // TODO: Temporary, should be improved in another branch
   return (
-    <div>
-      <table style={{backgroundColor: "orange", color: "white"}}>
-        <tbody>
-          <tr>
-            <td><OrangeInfoBox color="orange" /></td>
-            <td><OrangeInfoBox color="orangered" /></td>
-            <td><OrangeInfoBox color="orange" /></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <Row className="m-0">
+      <Col className="p-0"> <OrangeInfoBox {...DummyData} bg_color="orange"/> </Col>
+      <Col className="p-0"> <OrangeInfoBox {...DummyData} bg_color="orangered"/> </Col>
+      <Col className="p-0"> <OrangeInfoBox {...DummyData} bg_color="orange"/> </Col>
+    </Row>
   )
 }
 
 export default OrangeInfoBoxes;
 
 
-interface InfoBoxProps {
-  color: string
+type InfoBoxProps = {
+  title: string,
+  text?: ReactNode,
+  button_text: string,
+  bg_color: string
 }
 
 function OrangeInfoBox(props : InfoBoxProps) {
-    // TODO: Temporary, should be improved in another branch
   return (
-    <div className="" style={{padding: "1em", backgroundColor: props.color}}>
-        <h3>Lorem ipsum</h3>
-        <p style={{fontWeight: "lighter"}}>dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div className="px-5 py-4-5 h-100 text-white" style={{backgroundColor: props.bg_color}}>
+      <Centered><h3>{props.title}</h3></Centered>
+      {/* I didn't want to make a fixed height, I wanted a dynamic one. But couldn't find a way to put the buttons on the bottom all same height (y-position) */}
+      <div className="font-weight-light text-truncate" style={{height: "200px"}}>{props.text}</div>
+      <Centered><FButton text={props.button_text}/></Centered>
     </div>
   )
 }
