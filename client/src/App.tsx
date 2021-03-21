424953b5ede893c35c502b4600d9edb70aa9bd56
Ejemyr <37593295+Ejemyr@users.noreply.github.com>
2021-03-21 20:22:11 +0100

    Frontend lint config (#48)
    * Added initial linting.
    
    * Forced lint on all files.
    
    * Some fix?
    
    * Added lint scripts.
    
    * Added no-unused-vars and no-duplicate-imports to ESlint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,39 @@
-import React, {useState} from 'react'
-import { LocaleContext, locales } from './contexts'
-import Frontpage from './pages/Frontpage'
-import NewsArticlePage from './pages/NewsArticlePage'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Switch, Route } from "react-router-dom"
-import './App.css'
-import {DummyData2} from './components/news/FrontpageNewsWidget'
-import StyretPage from './pages/StyretPage'
-
+import React, { useState } from 'react';
+import { LocaleContext, locales } from './contexts';
+import Frontpage from './pages/Frontpage';
+import NewsArticlePage from './pages/NewsArticlePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+import { DummyData2 } from './components/news/FrontpageNewsWidget';
+import StyretPage from './pages/StyretPage';
 
 function App() {
-
-    const [locale, setLocale] = useState(locales.sv)
+    const [locale, setLocale] = useState(locales.sv);
 
     return (
         <div className="App">
-        <LocaleContext.Provider value={locale}>
+            <LocaleContext.Provider value={locale}>
+                <Header setLocale={setLocale}/>
+                <div className="content container">
+                    <Switch>
+                        <Route path="/styret">
+                            <StyretPage/>
+                        </Route>
 
-            <Header setLocale={setLocale}/>
+                        <Route path="/newsarticle">
+                            <NewsArticlePage {...DummyData2} />
+                        </Route>
 
-            <div className="content container">
-                <Switch>
-                  
-                    <Route path="/styret">
-                        <StyretPage/>
-                    </Route>
+                        <Route path="/">
+                            <Frontpage />
+                        </Route>
 
-                    <Route path="/newsarticle">
-                        <NewsArticlePage {...DummyData2} />
-                    </Route>
-
-                    <Route path="/">
-                        <Frontpage />
-                    </Route>
-
-                </Switch>
-            </div>
-
-            <Footer />
-
-        </LocaleContext.Provider>
+                    </Switch>
+                </div>
+                <Footer />
+            </LocaleContext.Provider>
         </div>
     );
 }
--- client/src/pages/NewsArticlePage.tsx
+++ client/src/pages/NewsArticlePage.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import NewsArticle, {INewsItem} from '../components/news/NewsItem';
-import MenuMonths from '../components/MenuMonths'
+import NewsArticle, { INewsItem } from '../components/news/NewsItem';
+import MenuMonths from '../components/MenuMonths';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-
 function NewsArticlePage(props: INewsItem) {
     return (
-      <Container className="pb-5">
-        <h1 className="pb-4">Nyheter</h1>
-        <Row>
-          <Col className="col-7 ml-5"> <NewsArticle {...props}/> </Col>
-          <Col className="col-2 ml-5"> <MenuMonths /> </Col>
-        </Row>
-      </Container>
-    )
-  }
-  
+        <Container className="pb-5">
+            <h1 className="pb-4">Nyheter</h1>
+            <Row>
+                <Col className="col-7 ml-5"> <NewsArticle {...props}/> </Col>
+                <Col className="col-2 ml-5"> <MenuMonths /> </Col>
+            </Row>
+        </Container>
+    );
+}
+
 export default NewsArticlePage;
