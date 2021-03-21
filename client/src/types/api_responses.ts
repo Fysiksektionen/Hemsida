f38903fc536e438dc5d45bbb5e8548c3b4f51762
Christoffer Ejemyr <ejemyr@kth.se>
2021-03-21 22:22:26 +0100

    Lint fixes and docstrings added.

--- client/src/types/api_responses.ts
+++ client/src/types/api_responses.ts
@@ -1,13 +1,18 @@
-
+/**
+ * File defining types returned in API from server.
+ *
+ * This file should only contain types that help us parse the raw response of the server.
+ * Do not add related types that are not returned by the server.
+ */
 
 export type APIResponse<T> = {
-    code: number,  // Status code
-    data: T        // Data of the response
+    code: number, // Status code
+    data: T // Data of the response
 }
 
 type IDDetail = {
     id: number,
-    detail_url: string
+    detailUrl: string
 }
 
 export type Error = {
@@ -21,23 +26,23 @@ export type MinimalPage = IDDetail & {
 
 export type Page = MinimalPage & {
     slug: string,
-    page_type: string,
+    pageType: string,
     parent: MinimalPage
     children: MinimalPage[],
     published: boolean,
-    published_at: string,
-    last_edited_at: string,
-    content_sv: object
-    content_en: object
+    publishedAt: string,
+    lastEditedAt: string,
+    contentSv: object
+    contentEn: object
 }
 
 export type Site = {
-    root_url: string,
-    root_page: Page,
-    banner_content_sv: object,
-    banner_content_en: object,
-    footer_content_sv: object,
-    footer_content_en: object
+    rootUrl: string,
+    rootPage: Page,
+    bannerContentSv: object,
+    bannerContentEn: object,
+    footerContentSv: object,
+    footerContentEn: object
 }
 
 export type MenuItem = {
@@ -46,7 +51,7 @@ export type MenuItem = {
     link: string,
     url?: string,
     page?: MinimalPage,
-    is_menu: boolean
+    isMenu: boolean
 }
 
 export type Menu = MenuItem & {
@@ -54,74 +59,72 @@ export type Menu = MenuItem & {
 }
 
 export type Redirect = IDDetail & {
-    from_path: string,
+    fromPath: string,
     link: string,
     url: string,
     page: MinimalPage
 }
 
-export type Image = IDDetail & {}  // More to be defined later
+export type Image = IDDetail & {} // More to be defined later
 
-
-type ContentObjectDBType = "text" | "image" | "menu" | "page" | "dict" | "list"
+type ContentObjectDBType = 'text' | 'image' | 'menu' | 'page' | 'dict' | 'list'
 
 type ContentObjectBase = IDDetail & {
     name: string,
-    db_type: ContentObjectDBType,
+    dbType: ContentObjectDBType,
     attributes: object
 }
 
 export type ContentObject = ContentText | ContentImage | ContentMenu | ContentPage | ContentDict | ContentList
 
 export type ContentText = ContentObjectBase & {
-    db_type: "text",
+    dbType: 'text',
     text: string
 }
 
 export type ContentImage = ContentObjectBase & {
-    db_type: "image",
+    dbType: 'image',
     image: Image
 }
 
 export type ContentMenu = ContentObjectBase & {
-    db_type: "menu",
+    dbType: 'menu',
     menu: Menu
 }
 
 export type ContentPage = ContentObjectBase & {
-    db_type: "page",
+    dbType: 'page',
     page: MinimalPage
 }
 
 export type ContentDict = ContentObjectBase & {
-    db_type: "dict",
+    dbType: 'dict',
     items: NodeJS.Dict<ContentObject>
 }
 
 export type ContentList = ContentObjectBase & {
-    db_type: "list",
+    dbType: 'list',
     items: ContentObject[]
 }
 
-
 export type User = IDDetail & {
-    "user_type": "" | "student" | "senior" | "external" | "admin",
-    "last_login": string,
-    "username": string,
-    "first_name": string,
-    "last_name": string,
-    "date_joined": string,
-    "is_active": boolean,
-    "kth_id": string,
-    "year": string,
-    "image": Image
-    "language": "sv" | "en",
-    "groups": Group[]
+    userType: '' | 'student' | 'senior' | 'external' | 'admin',
+    lastLogin: string,
+    username: string,
+    firstName: string,
+    lastName: string,
+    dateJoined: string,
+    isActive: boolean,
+    kthId: string,
+    year: string,
+    image: Image
+    language: 'sv' | 'en',
+    groups: Group[]
 }
 
 export type Group = IDDetail & {
-    "group_type": "" | "chapter_group" | "external" | "admin",
-    "name": string,
-    "description": string,
-    "image": Image
+    groupType: '' | 'chapter_group' | 'external' | 'admin',
+    name: string,
+    description: string,
+    image: Image
 }
--- client/src/types/general.ts
+++ client/src/types/general.ts
@@ -1,10 +1,12 @@
-import React from "react";
-import {Page} from "./api_responses";
+/**
+ * Types that are used by multiple files throughout the project.
+ */
 
+import React from 'react';
+import { Page } from './api_responses';
 
 export type PageComponentProps = {
     data: Page
 }
 
 export type PageComponent = React.FunctionComponent<PageComponentProps>;
-
