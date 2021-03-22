f1a53fa1f95ebb746a78e9bfdee8016794caac69
Christoffer Ejemyr <ejemyr@kth.se>
2021-03-22 01:54:56 +0100

    Revert "Fixed PageLoaderTypes using actual APIResponse types."
    This reverts commit 015d067c

--- client/src/types/general.ts
+++ client/src/types/general.ts
@@ -5,6 +5,8 @@
 import React from 'react';
 import { Page } from './api_object_types';
 
-export type PageComponentProps = Page
+export type PageComponentProps = {
+    data: Page
+}
 
 export type PageComponent = React.FunctionComponent<PageComponentProps>;
