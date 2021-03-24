2c936109ad264138fd7493579f3b916e7c195959
Christoffer Ejemyr <ejemyr@kth.se>
2021-03-25 00:32:47 +0100

    Started to setup form fixes.

--- client/src/admin/settings/Settings.tsx
+++ client/src/admin/settings/Settings.tsx
@@ -2,36 +2,48 @@ import React, { useState } from 'react';
 import { AdminPageProps } from '../../types/admin_components';
 import { Site } from '../../types/api_responses';
 import { Button } from 'react-bootstrap';
+import { Form, Field } from 'react-final-form';
+import callApi from '../call_api_temp';
 
 type FormState<T> = {
     hasChanged: boolean,
-    initialData: T,
-    currentData: T
+    initialData?: T,
+    currentData?: T
 }
 
 type SettingsAdminPageState = {
-    site: FormState<any>
+    site: FormState<Site>
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function SettingsAdminPage(props: AdminPageProps) {
     const [state, setState] = useState<SettingsAdminPageState>(
         {
-            site: { hasChanged: false, initialData: '', currentData: '' }
+            site: { hasChanged: false }
         }
     );
 
-    const siteUpdated = state.site.initialData;
+    async function onSiteFormSubmit() {
+        const resp = callApi({path: 'site/', getParams: {}});
+    }
 
     return (
         <div className="px-4 pt-4">
             <h1>Settings</h1>
             <hr />
-            <h2 className="d-flex flex-row justify-content-between">
-                <span>Site settings</span>
-                {state.site.initialData && <Button type="primary">Spara site settings</Button>}
-            </h2>
-
+            <Form onSubmit={onSiteFormSubmit}
+                render={({ handleSubmit, form, submitting, pristine, values }) => (
+                    <form onSubmit={handleSubmit}>
+                        <h2 className="d-flex flex-row justify-content-between">
+                            <span>Site settings</span>
+                            {state.site.hasChanged && <Button type="primary" > Spara site settings</>}
+                        </h2>
+                        <div>
+                            Field
+                        </div>
+                    </form>
+                )}
+            />
             <hr />
         </div>
     );
