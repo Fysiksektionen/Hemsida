e5b833b0e67ae24ee02a3a80ef056d675afc80a8
Christoffer Ejemyr <ejemyr@kth.se>
2021-03-25 02:35:46 +0100

    Some forms updates for validation etc.

--- client/src/admin/settings/Settings.tsx
+++ client/src/admin/settings/Settings.tsx
@@ -3,30 +3,22 @@ import { AdminPageProps } from '../../types/admin_components';
 import { APIResponse, Site } from '../../types/api_responses';
 import { Button, Col, Form } from 'react-bootstrap';
 import callApi from '../call_api_temp';
-import { adminMenuItems } from '../Admin';
 
+// TODO: Add current state updated onChange
 type FormState<T> = {
     hasChanged: boolean,
-    initialData: T,
-    currentData: T
+    initialData: T
 }
 
 type SettingsAdminPageState = {
     site: FormState<Site>
 }
 
-type FormValues = {
-    rootUrl: {
-        validated: boolean,
-
-    }
-}
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function SettingsAdminPage(props: AdminPageProps) {
     const [state, setState] = useState<SettingsAdminPageState>(() => {
         const data = (callApi({ path: 'site/', getParams: {} }) as APIResponse<Site>).data;
-        return ({ site: { hasChanged: false, initialData: data, currentData: data } });
+        return ({ site: { hasChanged: false, initialData: data } });
     });
 
     async function onSiteFormSubmit() {
@@ -36,22 +28,39 @@ export default function SettingsAdminPage(props: AdminPageProps) {
     const [validated, setValidated] = useState(false);
 
     const onChange = (event: ChangeEvent<any>) => {
-        const form = event.currentTarget;
-        if (!form.checkValidity()) {
-            event.preventDefault();
-            event.stopPropagation();
+        const formControl = event.target;
+
+        formControl.parentNode.classList.add('was-validated');
+        if (!state.site.hasChanged) {
+            console.log('setting new state');
+            setState({
+                site: {
+                    ...state.site,
+                    hasChanged: true
+                }
+            });
+            console.log(state);
         }
+        event.preventDefault();
+        event.stopPropagation();
     };
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         const form = event.currentTarget;
-        event.preventDefault();
 
-        if (!form.checkValidity()) {
-            event.stopPropagation();
+        if (form.checkValidity()) {
+            console.log('Updated!');
         }
         setValidated(true);
-        console.log('');
+        setState({
+            site: {
+                ...state.site,
+                hasChanged: false
+            }
+        });
+
+        event.preventDefault();
+        event.stopPropagation();
     };
 
     return (
@@ -61,18 +70,18 @@ export default function SettingsAdminPage(props: AdminPageProps) {
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <h2 className="row justify-content-between">
                     <span>Site settings</span>
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" type="submit" disabled={!state.site.hasChanged}>
                         <i className="fas fa-save" /> Save
                     </Button>
                 </h2>
                 <Form.Row>
-                    <Form.Group as={Col} md="4" controlId="validationCustom01">
-                        <Form.Label>First name</Form.Label>
+                    <Form.Group as={Col} md="6" controlId="validationCustom01">
+                        <Form.Label>Root URL</Form.Label>
                         <Form.Control
                             required
                             type="text"
                             placeholder="Root URL"
-                            defaultValue="Mark"
+                            defaultValue={state.site.initialData.rootUrl}
                             onChange={onChange}
                         />
                         <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
