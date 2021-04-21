998157f2e8dd63e1572efbbb9cf4445805feffec
Christoffer Ejemyr <ejemyr@kth.se>
2021-04-21 09:29:25 +0200

    Fixes scaling of NamndPage in edit mode

--- client/src/pages/NamndPage.tsx
+++ client/src/pages/NamndPage.tsx
@@ -4,6 +4,7 @@ import { NamndPageCT } from '../types/content_objects/content_trees/pages';
 import BlockFeedCOR from '../components/content_object_renderers/BlockFeedCOR';
 import { Col, Container, Row } from 'react-bootstrap';
 import BlockCOR from '../components/content_object_renderers/blocks/BlockCOR';
+import { EditorialModeContext } from '../contexts';
 
 /**
  * Page template of `namnd` pageType. Requires title and includes a BlockFeed as main content.
@@ -14,19 +15,23 @@ export default function NamndPage(props: ContentObject) {
     const content = props as NamndPageCT;
 
     return (
-        <Container>
-            <Row className='justify-content-center'>
-                <Col xl={8} md={10} xs={11}>
-                    <Row className='mb-5'>
-                        <h1>
-                            <BlockCOR block={content.items.title} />
-                        </h1>
+        <EditorialModeContext.Consumer>
+            {edit => (
+                <Container>
+                    <Row className='justify-content-center'>
+                        <Col xl={!edit ? 8 : 10} md={10} xs={11}>
+                            <Row className='mb-5'>
+                                <h1>
+                                    <BlockCOR block={content.items.title} />
+                                </h1>
+                            </Row>
+                            <Row>
+                                <BlockFeedCOR content={content.items.content} />
+                            </Row>
+                        </Col>
                     </Row>
-                    <Row>
-                        <BlockFeedCOR content={content.items.content} />
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
+                </Container>
+            )}
+        </EditorialModeContext.Consumer>
     );
 }
