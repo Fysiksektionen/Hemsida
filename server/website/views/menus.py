d980bd522c8b27edd4ef0b2b06686e3fd7858507
frigro <frigro@kth.se>
2021-05-09 09:44:56 +0200

    Split viewset into two, wrote (ish working) post-serializer.

--- server/website/views/menus.py
+++ server/website/views/menus.py
@@ -1,15 +1,21 @@
-from rest_framework import viewsets, mixins, generics
+from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import empty
+from rest_framework.relations import PrimaryKeyRelatedField
+from django.utils.translation import gettext_lazy as _
+
 from utils.serializers import DBObjectSerializer, OptionalHyperlinkedIdentityField
+from website.models.pages import Page
 from website.models.menus import Menu
 
 
-class MenuItemSerializer(DBObjectSerializer):
+class MenuItemGetSerializer(DBObjectSerializer):
     """Serializer for rendering MenuItem. Recursively renders menu.items with the same rendering as parent."""
     serializer_url_field = OptionalHyperlinkedIdentityField
 
     class Meta:
         model = Menu
-        fields = ['name', 'link', 'items', 'is_menu']
+        fields = ['name', 'link', 'page', 'url', 'items', 'is_menu']
         inf_depth = True
         extra_kwargs = {
             'detail_url': {
@@ -17,6 +23,9 @@ class MenuItemSerializer(DBObjectSerializer):
             }
         }
         nested_serialization = {
+            'page': {
+                'fields': ['name']
+            },
             'items': {
                 'use_base_meta': True,
                 'reuse_nested_serialization': True
@@ -24,13 +33,76 @@ class MenuItemSerializer(DBObjectSerializer):
         }
 
 
+class MenuItemPostSerializer(DBObjectSerializer):
+    """Serializer for posting MenuItem."""
+    page = PrimaryKeyRelatedField(queryset=Page.objects.all())
+
+    class Meta:
+        model = Menu
+        fields = ['name', 'url', 'page', 'is_menu', 'items']
+        inf_depth = True
+        nested_serialization = {
+            'items': {
+                'use_base_meta': True,
+                'reuse_nested_serialization': True
+            }
+        }
+
+    def __init__(self, instance=None, data=empty, *args, **kwargs):
+        if data is not empty:
+            items = data.pop('items', [])
+        super().__init__(instance, data, *args, **kwargs)
+        if data is not empty:
+            self.initial_data["items"] = items
+
+    def is_valid(self, raise_exception=False):
+        if not hasattr(self, '_validated_data'):
+            super().is_valid()
+            if self.initial_data['is_menu']:
+                items = self.initial_data["items"]
+                print(items)
+                if isinstance(items, list):
+                    self._serlist = list()
+                    for i in range(len(items)):
+                        items[i]["order"] = i
+                        print(type(MenuItemPostSerializer(data=items[i])))
+                        self._serlist.append(MenuItemPostSerializer(data=items[i]))
+                        if not self._serlist[i].is_valid():
+                            self._errors = {**self.errors, **self._serlist[i].errors}
+                else:
+                    self._errors = {**self.errors, **{"items": _("Has to be a list or empty")}}
+            if self._errors:
+                self._validated_data = {}
+                if hasattr(self, "_serlist"):
+                    self._serlist.clear()
+        if self._errors and raise_exception:
+            raise ValidationError(self.errors)
+
+        return not bool(self._errors)
+
+    def save(self, **kwargs):
+        super().save(**kwargs)
+        if hasattr(self, '_serlist'):
+            for ser in self._serlist:
+                ser.save(menu=self.instance)
+        return self.instance
+
+
 class MenusView(generics.ListCreateAPIView):
     """A simple view for listing and creating menus."""
-    serializer_class = MenuItemSerializer
     queryset = Menu.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == ('GET' or 'LIST'):
+            return MenuItemGetSerializer
+        return MenuItemPostSerializer
 
 
 class MenuView(generics.RetrieveUpdateDestroyAPIView):
     """A simple view for fetching, updating and deleting menus."""
-    serializer_class = MenuItemSerializer
     queryset = Menu.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MenuItemGetSerializer
+        return MenuItemPostSerializer
